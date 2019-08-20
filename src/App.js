4594cc3e734e3e577839d2fc95f54bb7e552f3fd
Yuttakhan Baingen <yuttakhanbaingen@gmail.com>
2019-08-20 20:59:41 +0700

    :sparkles: PromptpayQR generate from mobile phone & amount

--- src/App.js
+++ src/App.js
@@ -1,24 +1,33 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
+import QRCode from 'qrcode.react';
 import './App.css';
 
+const generatePayload = require('promptpay-qr');
+
 function App() {
+  const [ phoneNumber, setPhoneNumber ] = useState("012-345-6789");
+  const [ amount, setAmount ] = useState(1.00);
+  const [ qrCode ,setqrCode ] = useState("sample");
+
+  function handlePhoneNumber(e) {
+    setPhoneNumber(e.target.value);
+  }
+
+  function handleAmount(e) {
+    setAmount(parseFloat(e.target.value));
+  }
+
+  function handleQR() {
+    setqrCode(generatePayload(phoneNumber, { amount }));
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h2>I'm out of money so please donate me!</h2>
+      <input type="text" value={phoneNumber} onChange={handlePhoneNumber} />
+      <input type="number" value={amount} onChange={handleAmount} />
+      <button onClick={handleQR}>Generate Promptpay QR</button>
+      <QRCode value={qrCode} />
     </div>
   );
 }
