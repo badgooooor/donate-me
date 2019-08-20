e5cd10da637cc7f0ffdbb919e8146f42da0cae5e
Yuttakhan Baingen <yuttakhanbaingen@gmail.com>
2019-08-20 21:54:35 +0700

    :lipstick: Updating the UI

--- src/App.js
+++ src/App.js
@@ -1,9 +1,37 @@
 import React, { useState } from 'react';
 import QRCode from 'qrcode.react';
-import './App.css';
+import styled from 'styled-components';
 
 const generatePayload = require('promptpay-qr');
 
+const Title = styled.h1`
+  font-size: 1.5em;
+  text-align: center;
+  color: palevioletred;
+`
+
+const Container = styled.div`
+  height: 100vh;
+  padding: 4em;
+  background: papayawhip;
+`
+
+const FlexContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: space-around;
+`
+
+const QRWrapper = styled.div`
+  margin: auto;
+  padding: 10px;
+`
+
+const InputWrapper = styled.div`
+  margin: auto;
+  width: 200px;
+  padding: 10px;
+`
 function App() {
   const [ phoneNumber, setPhoneNumber ] = useState("012-345-6789");
   const [ amount, setAmount ] = useState(1.00);
@@ -22,13 +50,19 @@ function App() {
   }
 
   return (
-    <div>
-      <h2>I'm out of money so please donate me!</h2>
-      <input type="text" value={phoneNumber} onChange={handlePhoneNumber} />
-      <input type="number" value={amount} onChange={handleAmount} />
-      <button onClick={handleQR}>Generate Promptpay QR</button>
-      <QRCode value={qrCode} />
-    </div>
+    <Container>
+      <Title>I'm out of money so please donate me!</Title>
+      <FlexContainer>
+        <QRWrapper>
+          <QRCode value={qrCode} />
+        </QRWrapper>
+        <InputWrapper>
+          <input type="text" value={phoneNumber} onChange={handlePhoneNumber} />
+          <input type="number" value={amount} onChange={handleAmount} />
+          <button onClick={handleQR}>Generate Promptpay QR</button>
+        </InputWrapper>
+      </FlexContainer>
+    </Container>
   );
 }
 
